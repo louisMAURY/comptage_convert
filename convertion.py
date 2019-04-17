0748dada3e5477ec2c675f7ef0c9b711e236f4df
Louis <louismaury33@gmail.com>
2019-04-17 11:59:56 +0200

    On ne peut pas revenir en arrière !
    Le programme supprime le retour à la ligne "\n"

--- convertion.py
+++ convertion.py
@@ -1,4 +1,11 @@
 with open("dict-conv-chiffre.txt" , "r" , encoding = "utf8") as dictionary:
     content = dictionary.readlines()
 
-print(content)
+# The function remove de \n in element on "content" list
+def rm_return(the_dic):
+    for i in the_dic:
+        result = i.strip("\n")
+        print(result)
+    return result
+
+rm_return(content)
