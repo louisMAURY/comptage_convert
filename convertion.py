c229397c74c351cec9eab406b1495e5a48a23073
Louis <louismaury33@gmail.com>
2019-04-17 17:02:13 +0200

    "Je me suis cassé le traducteur"
    Le programme arrive à reconnaître ce qui est écrit en entrée par rapport au dictionnaire mais il n'arrive pas à le traduire en chiffre arabe...

--- convertion.py
+++ convertion.py
@@ -1,11 +1,29 @@
+list_number = []
+
 with open("dict-conv-chiffre.txt" , "r" , encoding = "utf8") as dictionary:
     content = dictionary.readlines()
 
-# The function remove de \n in element on "content" list
-def rm_return(the_dic):
-    for i in the_dic:
-        result = i.strip("\n")
-        print(result)
-    return result
 
-rm_return(content)
+
+
+
+user_input_literal = input("Entrez votre numéro (en lettre): ")
+
+def calcul(input_literal , the_dic):
+    for element in the_dic:
+        result = element.strip("\n")
+        list_number.append(result)
+
+    new_sentence = input_literal.replace(" " , "-")
+    worklist = new_sentence.split("-")
+
+    index_plus = 0
+    for word in worklist:
+        for number in list_number:
+            if word == number:
+                
+                print(find)
+            else:
+                print("ERREUR")
+
+calcul(user_input_literal , content)
